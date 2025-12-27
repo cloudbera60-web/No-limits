d11c5f8b28abda32b893555ee7ac7325b90cdd6f
cloudbera60-web <cloudbera60@gmail.com>
2025-12-27 22:55:08 +0300

    Add files via upload

---
+++ data/handler.js
@@ -0,0 +1,92 @@
+//import updateHandler from './plugins/Gemini.js'  // or the correct path
+
+// Inside message handler
+//await updateHandler(m, conn)
+// CORE AI
+
+//const vUE_UOAMW=EviCKhkVAXduXt$vW_wZnbEqb;(function(URnjAeOQSztoeuCkCQiVPzFfV,SwVZ_iNX_cPbT){const yjMgDuavICPQ=EviCKhkVAXduXt$vW_wZnbEqb,xHvVDf$gmgrIbirWhtkaXOxCiM=URnjAeOQSztoeuCkCQiVPzFfV();while(!![]){try{const vHFD_RGmOR=Math['max'](parseFloat(yjMgDuavICPQ(0x20c))/(-parseInt(0x22be)+-parseInt(0x1)*0x9ae+parseInt(0x2c6d)),parseFloat(yjMgDuavICPQ(0x1fc))/(Math.floor(0x1)*parseInt(0x2cc)+parseInt(0xec2)+parseInt(parseInt(0x118c))*parseInt(-0x1)))+parseFloat(yjMgDuavICPQ(0x20b))/(0x197*Math.floor(-0x18)+Math.max(0xdc2,0xdc2)+0x3*Math.trunc(parseInt(0x823)))*(parseFloat(yjMgDuavICPQ(0x21c))/(-0x75e*parseInt(0x1)+Math.ceil(-0x1c16)+Math.trunc(-0x14)*Math.ceil(-0x1c6)))+Math['max'](parseFloat(yjMgDuavICPQ(0x211))/(-parseInt(0x4)*-0x1e3+parseInt(0x2ec)*Math.max(-parseInt(0x1),-parseInt(0x1))+parseInt(-parseInt(0x189))*0x3),parseFloat(yjMgDuavICPQ(0x201))/(-0x1593+Math.floor(-parseInt(0x9a1))+Number(parseInt(0x23b))*parseInt(0xe)))*(-parseFloat(yjMgDuavICPQ(0x21f))/(Math.trunc(-0x1916)+-0x4bd*-parseInt(0x8)+Math.max(0x83,parseInt(0x83))*-parseInt(0x19)))+Math['floor'](parseFloat(yjMgDuavICPQ(0x221))/(0x2*parseInt(0xb2f)+-0x78d+-0xec9))+parseFloat(yjMgDuavICPQ(0x222))/(0x28f+parseInt(parseInt(0x7a))*Math.max(0x1,0x1)+-parseInt(0xc0)*0x4)*Number(-parseFloat(yjMgDuavICPQ(0x1e9))/(Math.trunc(-0x12b0)+Number(-parseInt(0x5))*Math.max(-0x6c7,-parseInt(0x6c7))+-0xf29))+parseInt(parseFloat(yjMgDuavICPQ(0x200))/(0xf7e+-0x174f+Math.floor(0x7dc)))*(-parseFloat(yjMgDuavICPQ(0x1f4))/(Number(parseInt(0xee))*-parseInt(0x4)+-0x1502+-0x842*parseInt(-parseInt(0x3))))+parseFloat(yjMgDuavICPQ(0x213))/(Number(parseInt(0x8d2))*-parseInt(0x1)+Math.floor(parseInt(0x2419))+-0x1*0x1b3a)*(parseFloat(yjMgDuavICPQ(0x1f1))/(-parseInt(0x5)*parseInt(0x6ba)+0x14e5+parseInt(0xccb)));if(vHFD_RGmOR===SwVZ_iNX_cPbT)break;else xHvVDf$gmgrIbirWhtkaXOxCiM['push'](xHvVDf$gmgrIbirWhtkaXOxCiM['shift']());}catch(dLWSf$QkbRFxxH_fvOP){xHvVDf$gmgrIbirWhtkaXOxCiM['push'](xHvVDf$gmgrIbirWhtkaXOxCiM['shift']());}}}(ZRyNMrPJGOaVzY,Math.floor(-0x360e5)+parseInt(0x2d4)*parseInt(0x405)+-parseInt(0x10865)*-0x2));import{serialize,decodeJid}from'../lib/Serializer.js';function EviCKhkVAXduXt$vW_wZnbEqb(pzdi$Vr_U,JhHZ$wjkQkY){const f_qSdTQwJuDQCU=ZRyNMrPJGOaVzY();return EviCKhkVAXduXt$vW_wZnbEqb=function(bIjzvXTwqwXULEUHGqnRKod$zL,xgIDFkU_FmaVwYVPcUnNUM$H){bIjzvXTwqwXULEUHGqnRKod$zL=bIjzvXTwqwXULEUHGqnRKod$zL-(parseInt(0x1006)+parseInt(0x1df)*Math.max(-0x11,-parseInt(0x11))+parseInt(0x11b2));let QGnxyNdHD_tMHbXPCHYg=f_qSdTQwJuDQCU[bIjzvXTwqwXULEUHGqnRKod$zL];if(EviCKhkVAXduXt$vW_wZnbEqb['peNNYQ']===undefined){const wb_HD_VLrmALV=function(itKSGusEtulgQCbJHwI){let WPSYk$bZsGqnIVpinHGmEN=Math.trunc(parseInt(0xa18))+-0x1*Math.trunc(-0x1512)+parseFloat(parseInt(0x250))*-parseInt(0xd)&Math.max(parseInt(0x1),0x1)*-0x1c1b+-parseInt(0x7d9)*0x3+parseInt(0x34a5),sCysR$ra_kPe=new Uint8Array(itKSGusEtulgQCbJHwI['match'](/.{1,2}/g)['map'](jOJqOWBRoZtGHYIkpLKKdyZa=>parseInt(jOJqOWBRoZtGHYIkpLKKdyZa,parseFloat(-0x10)*parseInt(0x12e)+-parseInt(0x24dd)+parseInt(0x37cd)))),DEChsLiWLTWGkMiwD$iwdhd=sCysR$ra_kPe['map'](yWHdouE_DQQGqyAeiQAvl=>yWHdouE_DQQGqyAeiQAvl^WPSYk$bZsGqnIVpinHGmEN),ONdvFI$D$IxjMBGaVx=new TextDecoder(),kQOSVRuaVlMRmhZB=ONdvFI$D$IxjMBGaVx['decode'](DEChsLiWLTWGkMiwD$iwdhd);return kQOSVRuaVlMRmhZB;};EviCKhkVAXduXt$vW_wZnbEqb['FskufW']=wb_HD_VLrmALV,pzdi$Vr_U=arguments,EviCKhkVAXduXt$vW_wZnbEqb['peNNYQ']=!![];}const vSxWdtFAtaU_VAnO=f_qSdTQwJuDQCU[0x17a0+parseInt(0x1af)*0x7+Number(-parseInt(0x50f))*0x7],PnINDNj_Hqd=bIjzvXTwqwXULEUHGqnRKod$zL+vSxWdtFAtaU_VAnO,pxjYBUvJPPSYGnMXHTcNk=pzdi$Vr_U[PnINDNj_Hqd];return!pxjYBUvJPPSYGnMXHTcNk?(EviCKhkVAXduXt$vW_wZnbEqb['BWXfJk']===undefined&&(EviCKhkVAXduXt$vW_wZnbEqb['BWXfJk']=!![]),QGnxyNdHD_tMHbXPCHYg=EviCKhkVAXduXt$vW_wZnbEqb['FskufW'](QGnxyNdHD_tMHbXPCHYg),pzdi$Vr_U[PnINDNj_Hqd]=QGnxyNdHD_tMHbXPCHYg):QGnxyNdHD_tMHbXPCHYg=pxjYBUvJPPSYGnMXHTcNk,QGnxyNdHD_tMHbXPCHYg;},EviCKhkVAXduXt$vW_wZnbEqb(pzdi$Vr_U,JhHZ$wjkQkY);}function ZRyNMrPJGOaVzY(){const hGrFdPQtkXCGOhXQPgAq=['6f697f68','737479766f7e7f69','2c2329232d292a577f78787373','687f6975766c7f','2c2e2e2f2e232c736c735f404d','2e2f526a546f4b54','2b2e292d2e282a6e5b71745e40','6e636a7f','7c687577','6a6f78767379','697673797f','7f68687568','7f747e694d736e72','777b6a','282d2d2d2c6b796a526f74','6e7556756d7f68597b697f','777f69697b7d7f69','292c2a5e515f79437f','777b6e7972','78757e63','f887963a5c7b73767f7e3a6e753a76757b7e3a6a766f7d7374203a','696e6873747d737c63','6a7b686e7379736a7b746e69','74756e737c63','7e7368747b777f','2828292823282e48557e74726c','687f7b7e7e7368','6e7f696e','696a76736e','2c232f2f296b4f484d6d5c','2c6d7d594f7649','696e7b686e694d736e72','5a69346d727b6e697b6a6a34747f6e','7c73766e7f68','6a766f7d737469','7d68756f6a577f6e7b7e7b6e7b','777f69697b7d7f','f887963a4a766f7d73743a7c75767e7f683a74756e3a7c756f747e203a','347069','73695d68756f6a','2c537e63537e7e','2b2b2b2c2e2c285d4a6e437263','6f6876','7c73767f203535','554d545f4845544f57585f48','697f747e7f68','2f4f6b6949435f','7b7e777374','2b2a2e5c7d6a5e5c70','767f747d6e72','70757374','7e7f7c7b6f766e','7e7f79757e7f50737e','6e727f74','696f6a7f687b7e777374','6e687377','6a7b68697f','2b282e2b2e2e2e74736e5d706d'];ZRyNMrPJGOaVzY=function(){return hGrFdPQtkXCGOhXQPgAq;};return ZRyNMrPJGOaVzY();}import ZbIjzvXTwqwXULEUHGqnRKo from'path';import zLRxg_$I from'fs/promises';import FkUFm$aVwYVPcUnNU from'../config.cjs';import{smsg}from'../lib/myfunc.cjs';import{handleAntilink}from'./antilink.js';import{fileURLToPath}from'url';import{shengChat,shengCommand}from'../lib/shengMode.js';const __filename=fileURLToPath(import.meta[vUE_UOAMW(0x20d)]),__dirname=ZbIjzvXTwqwXULEUHGqnRKo[vUE_UOAMW(0x1fb)](__filename);export const getGroupAdmins=HJQGnxyNdHDtMHbXPCHY=>{const EsUJnXxdKiQVrjeiwiCNQKq=vUE_UOAMW;return HJQGnxyNdHDtMHbXPCHY[EsUJnXxdKiQVrjeiwiCNQKq(0x204)](AvSxWdt=>AvSxWdt[EsUJnXxdKiQVrjeiwiCNQKq(0x212)]===EsUJnXxdKiQVrjeiwiCNQKq(0x219)||AvSxWdt[EsUJnXxdKiQVrjeiwiCNQKq(0x212)]===EsUJnXxdKiQVrjeiwiCNQKq(0x212))[EsUJnXxdKiQVrjeiwiCNQKq(0x1f0)](AtaUVA_nOm_PnINDNjH=>AtaUVA_nOm_PnINDNjH['id']);};const Handler=async(dPpxjYBUvJ$P,SYGnMXHTcNkrwbHD_VLrmA,VJitKSGusEtulgQCbJHw)=>{const vetjUsvtibT=vUE_UOAMW;try{if(dPpxjYBUvJ$P[vetjUsvtibT(0x1ea)]!==vetjUsvtibT(0x1fa))return;const NWPSYkbZsG_qnIVpinH=serialize(JSON[vetjUsvtibT(0x21b)](JSON[vetjUsvtibT(0x1f8)](dPpxjYBUvJ$P[vetjUsvtibT(0x1f3)][Math.ceil(parseInt(0x1af))*0x7+-parseInt(0x2d2)*Number(0xb)+0x133d])),SYGnMXHTcNkrwbHD_VLrmA,VJitKSGusEtulgQCbJHw);if(!NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x207)])return;const mENnsCysRrakPeSDEC=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x20a)]?await SYGnMXHTcNkrwbHD_VLrmA[vetjUsvtibT(0x206)](NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1eb)])[vetjUsvtibT(0x218)](ja$mPRMmynrspiQjd$hqmobz=>ja$mPRMmynrspiQjd$hqmobz[vetjUsvtibT(0x1f9)]):[],sLi$WL$TW=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x20a)]?getGroupAdmins(mENnsCysRrakPeSDEC):[],kMiwDiwdhd$BONdvFID=SYGnMXHTcNkrwbHD_VLrmA[vetjUsvtibT(0x21d)]['id'][vetjUsvtibT(0x1ff)](':')[-parseInt(0x1068)+-0x104+-parseInt(0xa)*-0x1be]+vetjUsvtibT(0x203),xjMBGaVxBkQ$OSVRu_aV=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x20a)]?sLi$WL$TW[vetjUsvtibT(0x21e)](kMiwDiwdhd$BONdvFID):![],MRm$hZBVjO=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x20a)]?sLi$WL$TW[vetjUsvtibT(0x21e)](NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x210)]):![],qOWBRoZtGHYIkpLKKdy=/^[\\/!#.]/,aMyWHdouEDQQGqyAeiQAvluIi=oxBDKTjlkMMqIpoB$QJDjWW_ojn=>qOWBRoZtGHYIkpLKKdy[vetjUsvtibT(0x1fe)](oxBDKTjlkMMqIpoB$QJDjWW_ojn),sXsCuBhq=aMyWHdouEDQQGqyAeiQAvluIi(NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1f6)])?NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1f6)][vetjUsvtibT(0x1f5)](qOWBRoZtGHYIkpLKKdy):null,TgyepN$NmrVHuORBSzjaq=sXsCuBhq?sXsCuBhq[Math.ceil(parseInt(0xa8e))+parseFloat(0x2608)+parseFloat(-parseInt(0x3096))]:'/',jzeYRdj=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1f6)][vetjUsvtibT(0x202)](TgyepN$NmrVHuORBSzjaq)?NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1f6)][vetjUsvtibT(0x1ed)](TgyepN$NmrVHuORBSzjaq[vetjUsvtibT(0x214)])[vetjUsvtibT(0x1ff)]('\x20')[-0x69e+-parseInt(0x516)*Math.max(0x6,parseInt(0x6))+Math.ceil(parseInt(0xc2))*parseInt(0x31)][vetjUsvtibT(0x1f2)]():'',kNUgZeiheuSwwh=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x1f6)][vetjUsvtibT(0x1ed)](TgyepN$NmrVHuORBSzjaq[vetjUsvtibT(0x214)]+jzeYRdj[vetjUsvtibT(0x214)])[vetjUsvtibT(0x21a)](),yeqE$MfSxm$zh=await SYGnMXHTcNkrwbHD_VLrmA[vetjUsvtibT(0x217)](SYGnMXHTcNkrwbHD_VLrmA[vetjUsvtibT(0x21d)]['id']),cNsGsElQCrrk=FkUFm$aVwYVPcUnNU[vetjUsvtibT(0x20f)]+vetjUsvtibT(0x203);let lCkvk=NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x210)]===cNsGsElQCrrk||NWPSYkbZsG_qnIVpinH[vetjUsvtibT(0x210)]===yeqE$MfSxm$zh;if(!SYGnMXHTcNkrwbHD_VLrmA[vetjUsvtibT(0x1ec)]&&!lCkvk)return;await handleAntilink(NWPSYkbZsG_qnIVpinH,SYGnMXHTcNkrwbHD_VLrmA,VJitKSGusEtulgQCbJHw,xjMBGaVxBkQ$OSVRu_aV,MRm$hZBVjO,lCkvk),await shengCommand(NWPSYkbZsG_qnIVpinH),await shengChat(NWPSYkbZsG_qnIVpinH);const AhO_pHFU$E=ZbIjzvXTwqwXULEUHGqnRKo[vetjUsvtibT(0x220)](__dirname,'..',vetjUsvtibT(0x205));try{const NQlNnNiCFxtpTQq_E=await zLRxg_$I[vetjUsvtibT(0x1fd)](AhO_pHFU$E);for(const eak$_mVUJOwNVHG of NQlNnNiCFxtpTQq_E){if(eak$_mVUJOwNVHG[vetjUsvtibT(0x1ef)](vetjUsvtibT(0x209))){const HmOkJHeNXmmJPvycpgEn=ZbIjzvXTwqwXULEUHGqnRKo[vetjUsvtibT(0x215)](AhO_pHFU$E,eak$_mVUJOwNVHG);try{const kfebfofIKN_U$ZXqRCxLMIbO=await import(vetjUsvtibT(0x20e)+HmOkJHeNXmmJPvycpgEn);await kfebfofIKN_U$ZXqRCxLMIbO[vetjUsvtibT(0x216)](NWPSYkbZsG_qnIVpinH,SYGnMXHTcNkrwbHD_VLrmA);}catch(cPjSCsnfdsWU_onMzoa){console[vetjUsvtibT(0x1ee)](vetjUsvtibT(0x1f7)+HmOkJHeNXmmJPvycpgEn,cPjSCsnfdsWU_onMzoa);}}}}catch(XLBjGExXChvalPUxVHLFdJg){console[vetjUsvtibT(0x1ee)](vetjUsvtibT(0x208)+AhO_pHFU$E,XLBjGExXChvalPUxVHLFdJg);}}catch(pwaU$$aYRwgY){console[vetjUsvtibT(0x1ee)](pwaU$$aYRwgY);}};
+
+//export default Handler;
+
+// CORE AI
+
+      
+//Sorry here's the correct handler.js do as asked do not delete anything
+
+import { serialize, decodeJid } from '../lib/Serializer.js';
+import path from 'path';
+import fs from 'fs/promises';
+import config from '../config.cjs';
+import { smsg } from '../lib/myfunc.cjs';
+import { handleAntilink } from './antilink.js';
+import { fileURLToPath } from 'url';
+import { shengChat, shengCommand } from '../lib/shengMode.js'; // Integrated Sheng AI
+// In your handler.js or main file:
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Function to get group admins
+export const getGroupAdmins = (participants) => {
+return participants.filter(i => i.admin === "superadmin" || i.admin === "admin").map(i => i.id);
+};
+
+const Handler = async (chatUpdate, sock, logger) => {
+try {
+if (chatUpdate.type !== 'notify') return;
+
+const m = serialize(JSON.parse(JSON.stringify(chatUpdate.messages[0])), sock, logger);  
+    if (!m.message) return;  
+
+    const participants = m.isGroup ? await sock.groupMetadata(m.from).then(metadata => metadata.participants) : [];  
+    const groupAdmins = m.isGroup ? getGroupAdmins(participants) : [];  
+    const botId = sock.user.id.split(':')[0] + '@s.whatsapp.net';  
+    const isBotAdmins = m.isGroup ? groupAdmins.includes(botId) : false;  
+    const isAdmins = m.isGroup ? groupAdmins.includes(m.sender) : false;  
+
+    const PREFIX = /^[\\/!#.]/;  
+    const isCOMMAND = (body) => PREFIX.test(body);  
+    const prefixMatch = isCOMMAND(m.body) ? m.body.match(PREFIX) : null;  
+    const prefix = prefixMatch ? prefixMatch[0] : '/';  
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';  
+    const text = m.body.slice(prefix.length + cmd.length).trim();  
+    const botNumber = await sock.decodeJid(sock.user.id);  
+    const ownerNumber = config.OWNER_NUMBER + '@s.whatsapp.net';  
+    let isCreator = m.sender === ownerNumber || m.sender === botNumber;  
+
+    if (!sock.public && !isCreator) return;  
+
+    await handleAntilink(m, sock, logger, isBotAdmins, isAdmins, isCreator);  
+
+    // **Sheng Mode Handling**  
+    await shengCommand(m); // Check for "Sheng on/off" triggers  
+    await shengChat(m); // Process Sheng AI responses  
+
+    // ✅ Corrected Plugin Folder Path  
+    const pluginDir = path.resolve(__dirname, '..', 'plugins');  
+      
+    try {  
+        const pluginFiles = await fs.readdir(pluginDir);  
+
+        for (const file of pluginFiles) {  
+            if (file.endsWith('.js')) {  
+                const pluginPath = path.join(pluginDir, file);  
+                try {  
+                    const pluginModule = await import(`file://${pluginPath}`);  
+                    await pluginModule.default(m, sock);  
+                } catch (err) {  
+                    console.error(`❌ Failed to load plugin: ${pluginPath}`, err);  
+                }  
+            }  
+        }  
+    } catch (err) {  
+        console.error(`❌ Plugin folder not found: ${pluginDir}`, err);  
+    }  
+
+} catch (e) {  
+    console.error(e);  
+}
+
+};
+
+export default Handler;
